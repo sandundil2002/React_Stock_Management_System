7d67244ee4ad93a7eaf717f14353ba63b56104be
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-12-28 22:02:08 +0530

    customer buttons component implemented.

--- src/pages/CustomerPage.tsx
+++ src/pages/CustomerPage.tsx
@@ -1,11 +1,13 @@
 import {HeaderComponent} from "../components/header/HeaderComponent.tsx";
 import {InputFieldsComponent} from "../components/customer/InputFieldsComponent.tsx";
+import {ButtonComponent} from "../components/customer/ButtonComponent.tsx";
 
 export const CustomerPage = () => {
     return (
         <>
             <HeaderComponent section="Customer Section" />
             <InputFieldsComponent />
+            <ButtonComponent />
         </>
     );
 };
